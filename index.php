2bf270861dcd62ff3ae5975f282051f35d745b05
gpz <raisedabar@yahoo.com>
2015-06-10 14:38:32 -1000

    initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Aloha Photo Contest, Members Photos Only, 2016 Calendar">
+    <meta name="author" content="Greg Zentkovich">
+    <link rel="icon" href="">
+
+    <title>Aloha Pacific Photo Contest</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/signin.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.min.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+  <div class="site-wrapper">
+
+    <div class="site-wrapper-inner">
+
+    <div class="cover-container">
+
+      <div class="header clearfix">
+        <h1>Aloha Pacific FCU Photo Contest</h1>
+        <p>Please sign in using your email, full name, and the last 3-digits of your Primary Account.</p>
+      </div>
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputFirstName" class="sr-only">First Name</label>
+        <input type="text" id="inputFirstName" class="form-control" placeholder="First Name" required autofocus>
+        <label for="inputLastName" class="sr-only">Last Name</label>
+        <input type="text" id="inputLastName" class="form-control" placeholder="Last Name" required autofocus>
+        <label for="inputLast3" class="sr-only">Last 3</label>
+        <input type="password" id="inputLast" class="form-control" placeholder="Last 3 digits of your Account" required>
+        <button class="btn btn-lg btn-default btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /cover-container -->
+  </div> <!-- /site-wrapper-inner -->
+</div> <!-- /site-wrapper -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
